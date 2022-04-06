1ed1b6f9949792cc5aa5ccc728c2f231764d419b
Krystian Wiese <wiesekrystian93@gmail.com>
2022-04-06 22:36:02 +0200

    added statelss functional component

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import Counter from './counterComponent'
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            {id:1,value:4},
-            {id:2,value:0},
-            {id:3,value:0},
-            {id:4,value:0}
-        ]
-     };
+
     
-     handleDelete = () => {
-        console.log('ev h called');
-    };
+
     render() { 
-        console.log('props: ',this.props)
         return (
         <div>
-            { this.state.counters.map(counter => 
-            <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value}/>
+            <button 
+            onClick={this.props.onReset}
+            className="btn btn-primary btn-sm m-2"
+            >
+            Reset
+            </button>
+
+            {this.props.counters.map(counter => 
+            <Counter 
+                key={counter.id} 
+                onDelete={this.props.onDelete}
+                onIncrement={this.props.onIncrement}
+                counter = {counter}
+            />
             )}
         </div>
         );
